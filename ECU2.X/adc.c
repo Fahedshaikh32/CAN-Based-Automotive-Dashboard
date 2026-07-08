bdd4715312625e522c4a162160c6bee0dff1cf4d
Fahed Shaikh <124694357+Fahedshaikh32@users.noreply.github.com>
2026-07-09 00:27:17 +0530

    Add files via upload

---
+++ ECU2.X/adc.c
@@ -0,0 +1,39 @@
+#include <xc.h>   // include microcontroller header
+#include "adc.h"   // include ADC header
+
+void init_adc(void)   // function to initialize ADC
+{
+	ADFM = 1;   // set result right justified
+
+	ACQT2 = 0;   // acquisition time bit 2
+	ACQT1 = 1;   // acquisition time bit 1
+	ACQT0 = 0;   // acquisition time bit 0
+
+	ADCS0 = 0;   // ADC clock bit 0
+	ADCS1 = 1;   // ADC clock bit 1
+	ADCS2 = 0;   // ADC clock bit 2
+
+	GODONE = 0;   // stop conversion initially
+
+	VCFG1 = 0;   // set VSS as reference
+	VCFG0 = 0;   // set VDD as reference
+
+	ADRESH = 0;   // clear high register
+	ADRESL = 0;   // clear low register
+
+	ADON = 1;   // turn ON ADC
+}
+
+unsigned short read_adc(unsigned char channel)   // function to read ADC value
+{
+	unsigned short reg_val;   // variable to store result
+
+	ADCON0 = (ADCON0 & 0xC3) | (channel << 2);   // select ADC channel
+
+	GO = 1;   // start conversion
+	while (GO);   // wait until conversion complete
+
+	reg_val = (ADRESH << 8) | ADRESL;   // combine result
+
+	return reg_val;   // return value
+}
---
+++ ECU2.X/build/default/production/ecu_2_main.p1.d
@@ -0,0 +1,8 @@
+build/default/production/ecu_2_main.p1:  \
+ecu_2_main.c  \
+ecu2_sensor.h  \
+digital_keypad.h  \
+adc.h  \
+can.h  \
+msg_id.h  \
+uart.h 
